59ba0b050a6bc0c4b319b26544004e3d4437e546
BuildTools <unconfigured@null.spigotmc.org>
2021-06-26 16:57:31 -0700

    RESTful APIs draft completed

--- MiddlewarePython/restful_apis.py
+++ MiddlewarePython/restful_apis.py
@@ -5,7 +5,6 @@ Middleware Logic - Python RESTful APIs
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import sqlite3
-import ast
 
 # Create Flask app / api
 app = Flask(__name__)
@@ -56,7 +55,7 @@ class BranchInfo(Resource):
         branches = cursor.fetchall()
         connection.close()
 
-        if branches is None:
+        if branches is None or not len(branches):
             return [], 404
         else:
             branch_list = []
@@ -70,7 +69,30 @@ class BranchInfo(Resource):
 class SectionInfo(Resource):
     
     def get(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument('branch', required=True)
+
+        args = parser.parse_args()
+        branch = args['branch']
+
+        connection = sqlite3.connect('./TestDatabase/testdb.db')
+        cursor = connection.cursor()
+
+        cursor.execute(f'''
+        SELECT SectionInfo.section FROM SectionInfo JOIN BranchInfo ON \
+            '{branch}' = BranchInfo.branch AND BranchInfo.id = SectionInfo.branch_id
+        ''')
+
+        sections = cursor.fetchall()
+        connection.close()
+
+        if sections is None or not len(sections):
+            return [], 404
+        else:
+            section_list = []
+            for sect in sections:
+                section_list.append(sect[0])
+            return section_list, 200        
 
 
 
@@ -78,15 +100,100 @@ class SectionInfo(Resource):
 class StudentInfo(Resource):
 
     def get(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument('branch', required=True)
+        parser.add_argument('section', required=True)
+
+        args = parser.parse_args()
+        branch = args['branch']
+        section = args['section']
+
+        connection = sqlite3.connect('./TestDatabase/testdb.db')
+        cursor = connection.cursor()
+
+        cursor.execute(f'''
+        SELECT StudentInfo.name FROM StudentInfo JOIN SectionInfo JOIN BranchInfo ON \
+            '{branch}' = BranchInfo.branch AND BranchInfo.id = StudentInfo.branch_id AND '{section}' = SectionInfo.section \
+                AND SectionInfo.id = StudentInfo.section_id
+        ''')
+
+        students = cursor.fetchall()
+        connection.close()
+
+        if students is None or not len(students):
+            return [], 404
+        else:
+            student_list = []
+            for stu in students:
+                student_list.append(stu[0])
+            return student_list, 200
 
 
 
 # Submit Attendance Info (POST)
 class SubmitAttendance(Resource):
 
+    # Student ID function for submitting attendance
+    def getStudentID(self, branch, section, name):
+        connection = sqlite3.connect('./TestDatabase/testdb.db')
+        cursor = connection.cursor()
+
+        cursor.execute(f'''
+        SELECT StudentInfo.id FROM StudentInfo JOIN BranchInfo JOIN SectionInfo ON '{branch}' = BranchInfo.branch \
+            AND BranchInfo.id = StudentInfo.branch_id AND '{section}' = SectionInfo.section AND SectionInfo.id \
+            = StudentInfo.section_id AND '{name}' = StudentInfo.name
+        ''')
+
+        result = cursor.fetchall()
+        connection.close()
+
+        if result is None or len(result) == 0 or len(result) > 1:
+            return None
+        else:
+            return result[0][0]
+
     def post(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument('branch', required=True)
+        parser.add_argument('section', required=True)
+        parser.add_argument('name', required=True)
+        parser.add_argument('date', required=True)
+        parser.add_argument('status', required=True)
+
+        args = parser.parse_args()
+        branch = args['branch']
+        section = args['section']
+        name = args['name']
+        date = args['date']
+        status = args['status']
+
+        connection = sqlite3.connect('./TestDatabase/testdb.db')
+        cursor = connection.cursor()
+
+        id = self.getStudentID(branch, section, name)
+        if id is None:
+            return "Failed to submit", 404
+
+        cursor.execute(f'''
+        SELECT AttendanceInfo.student_id FROM AttendanceInfo WHERE {id} = AttendanceInfo.student_id AND \
+            '{date}' = AttendanceInfo.date_id
+        ''')
+
+        if len(cursor.fetchall()):
+            cursor.execute(f'''
+            UPDATE AttendanceInfo SET status = '{status}' \
+                WHERE {id} = AttendanceInfo.student_id AND '{date}' = AttendanceInfo.date_id
+            ''')
+            connection.commit()
+            connection.close()
+            return "Successfully updated", 200
+        else:
+            cursor.execute(f'''
+            INSERT INTO AttendanceInfo (student_id, date_id, status) VALUES ({id}, '{date}', '{status}')
+            ''')
+            connection.commit()
+            connection.close()
+            return "Successfully submitted", 200
 
 
 
